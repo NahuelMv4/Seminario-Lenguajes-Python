1f8eae485c2ecd3fb002699a861108a95b53d25e
Nahuel Saroglia Gambino <nahuelgambino@gmail.com>
2024-06-19 16:13:27 -0300

    Subo el procesamiento de datos

---
+++ Trabajo Integrador/data_process/conectividad.py
@@ -0,0 +1,29 @@
+def reemplazar_valores(fila):
+    """
+    Reemplaza los valores '--' por 'NO' en una fila del dataset.
+
+    Argsumentos:
+        fila: Lista que representa una fila del dataset.
+
+    Retorna:
+        None
+    """
+    for i, valor in enumerate(fila):
+        if valor == "--":
+            fila[i] = "NO"
+
+def tiene_conectividad(fila):
+    """
+    Determina si una fila posee conectividad.
+
+    Args:
+        fila: Lista que representa una fila del dataset.
+
+    Retorna:
+        Un String 'SI' si la fila posee al menos un tipo de conectividad, 'NO' en caso contrario.
+    """
+    conectividad = fila[4:13]  # Tomar solo los campos relevantes
+    for dato in conectividad:
+        if dato != "NO":
+            return "SI"
+    return "NO"
